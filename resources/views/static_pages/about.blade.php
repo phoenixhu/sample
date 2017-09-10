567a378c4fb6e49aec49d619d6f8baa6e5792d70
tiange <phoenix_hu@outlook.com>
2017-09-10 16:11:08 +0000

    Finish static pages

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sample App</title>
+</head>
+<body>
+<h1>关于页</h1>
+</body>
+</html>
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sample App</title>
+</head>
+<body>
+<h1>帮助页</h1>
+</body>
+</html>
