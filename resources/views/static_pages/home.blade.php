b4afa63edca6530b30aa2da9cf810007b9ed2975
tiange <phoenix_hu@outlook.com>
2017-09-11 07:36:11 +0000

    Finish layout and routess

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -7,7 +7,7 @@
             你现在所看到的是 <a href="https://fsdhub.com/books/laravel-essential-training-5.1">Laravel 入门教程</a> 的示例项目主页.
         </p>
         <p>
-            一切，将从这里开始。
+            一切，将从这里开始.
         </p>
         <p>
             <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
